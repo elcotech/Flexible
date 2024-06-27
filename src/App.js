ef23f858de2fcf2729b54f799d16fc8d91738ee4
Mequanent <mequmail@gmail.com>
2024-06-27 19:21:19 +0300

    Deploying original project to Vercel

--- src/App.js
+++ src/App.js
@@ -65,7 +65,7 @@ function App() {
               <input type="text" id="name" name="name" required /><br/>
               <label htmlFor="email">Email</label>
               <input type="email" id="email" name="email" required /><br/>
-              <label htmlFor="message">Message</label>
+              <label htmlFor="message">Message</label><br/><br/><br/>
               <textarea id="message" name="message" required></textarea>
               <button type="submit">Send</button>
             </form>
