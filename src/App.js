df050d47fd80666d367fa3005a4e0f5e330a73ac
Mequanent <mequmail@gmail.com>
2024-07-04 15:17:51 +0300

    ELCOTECH

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   const content = {
     //home: 'Home',
-    about: 'About ELCOTECH',
+    about: 'About',
     services: 'Services',
     testimonials: 'Testimonials',
     contact: 'Contact',
