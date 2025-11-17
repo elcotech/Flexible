c0970a27b5073237ee6c644abdc4efa8a0cc281b
elcotech <mequmail@gmail.com>
2025-11-17 21:06:31 +0300

    Resize hero image to 60% width

--- src/App.js
+++ src/App.js
@@ -54,7 +54,7 @@ function App() {
           </ul>
         </nav>
         <div className="hero-image">
-          <img src="2.jpg" alt="Hero" style={{ width: '100%', height: 'auto' }} />
+          <img src="2.jpg" alt="Hero" style={{ width: '60%', height: 'auto' }} />
         </div>
       </header>
       <main>
